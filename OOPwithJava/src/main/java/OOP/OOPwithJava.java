dae37eb5bf57c9cc4a634428bfb111ac5b7692a6
Masud <abdullahalmasudkhan@gmail.com>
2024-11-08 00:25:05 +0600

    Started OOP Journey

---
+++ OOPwithJava/src/main/java/OOP/OOPwithJava.java
@@ -0,0 +1,10 @@
+package OOP;
+
+public class OOPwithJava {
+
+    public static void main(String[] args) {
+        System.out.println("Hello This is Masud.");
+        System.out.println("I will learn OOP with Java in this Java Project.");
+        System.out.println("Lets begin the wonderful journey with OOP to explore the real word of coding.");
+    }
+}
